805a1b9a05d8b1628a8325b546a17f8d1ef1cb93
rafaelmaia-dev <rafaelnetomaiar@gmail.com>
2026-06-21 18:45:40 -0300

    Conclusão do arquivo writer.py. features: __init__ - valida vault e inicializa _build_path - sanitiza nomes e constrói o caminho weite - delega, grava, loga e retorna

--- src/obsidian/writer.py
+++ src/obsidian/writer.py
@@ -14,12 +14,26 @@ class ObsidianWriter:
         logger.info(f"ObsidianWriter inicializado. Vault: {self.vault_path}")
 
 
-    def _build_path(self, space_name: str, folder_name: str, task_name: str) -> Path
-        # if folder_name.exists()
-            pasta.mkdir(parents=True, exist_ok=True)
-        # else:
+    def _build_path(self, space_name: str, folder_name: str, task_name: str) -> Path:
+         space_name = re.sub(r'[<>:"/\\|?*]', '', space_name)
+         folder_name = re.sub(r'[<>:"/\\|?*]', '', folder_name)
+         task_name = re.sub(r'[<>:"/\\|?*]', '', task_name)
 
-       
+         pasta = self.vault_path / "ClickUp" / space_name / folder_name
+         pasta.mkdir(parents=True, exist_ok=True) 
+         return pasta / f"{task_name}.md"
+
+
+    def write(self, space_name: str, folder_name: str, task_name: str, content: str) -> Path:
+
+        file_path = self._build_path(space_name, folder_name, task_name)
+        file_path.write_text(content, encoding="utf-8")
+
+        logger.info(f"{file_path} Salvo com sucesso! ")
+
+        return file_path
+        
+      
 if __name__ == "__main__":
      writer = ObsidianWriter(Config.OBSIDIAN_VAULT_PATH)
 
